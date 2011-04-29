de3a98b02a8cb09441f2a858cebff11b57adca21
Jan Paul Posma <jp.posma@gmail.com>
2011-04-29 14:57:15 +0200

    Added visualisation

---
+++ rm/src/rm/Main.java
@@ -0,0 +1,33 @@
+/* test program for displaying the AST */
+
+package rm;
+
+import rm.lexer.*;
+import rm.parser.*;
+import rm.node.*;
+import rm.*;
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class Main {
+    public static void main (String[] arg) {
+	try {
+	    System.out.println("This is a test, type a legal expression:");
+	    // Create a parser
+        Parser p = new Parser(
+                       new Lexer(
+                       new PushbackReader(
+                       new InputStreamReader(System.in), 1024)));
+
+        // parse the input
+         Start tree = p.parse();
+         ASTDisplay ad = new ASTDisplay ();
+         tree.apply (ad);
+            
+        } catch (Exception e) {
+            System.out.println("Exception occurred: "+e.getMessage());
+        }
+    }
+}
+
