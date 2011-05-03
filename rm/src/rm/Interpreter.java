eccc9ea68e7518d04f30e079545c8edcc0e5222a
Roan Kattouw <roan.kattouw@gmail.com>
2011-05-03 14:36:11 +0200

    Reorder stuff in Interpreter to match the grammar

--- rm/src/rm/Interpreter.java
+++ rm/src/rm/Interpreter.java
@@ -8,12 +8,60 @@ import rm.node.*;
 public class Interpreter extends DepthFirstAdapter {
 	Context globalContext = new Context();
 	
-	public void outAIntFactor(AIntFactor node) {
-		setOut(node, new IntValue(Long.valueOf(node.getIntdenotation().getText())));
+	public void outALetDef(ALetDef node) {
+		LinkedList<String> names = new LinkedList<String>();
+		PParlst parlst = node.getParlst();
+		if (parlst instanceof ANonemptyParlst) {
+			PPars pars = ((ANonemptyParlst)parlst).getPars();
+			while (pars instanceof AMultiPars) {
+				names.addFirst(((AMultiPars)pars).getIdent().getText());
+				pars = ((AMultiPars)pars).getPars();
+			}
+			names.addFirst(((ASinglePars)pars).getIdent().getText());
+		}
+		globalContext.putFuncDef(node.getIdent().getText(), new FuncDef(names, (Value)getOut(node.getExpr())));
 	}
 	
-	public void outARealFactor(ARealFactor node) {
-		setOut(node, new DoubleValue(Double.valueOf(node.getRealdenotation().getText())));
+	public void outAExprComp(AExprComp node) {
+		System.out.println(((Value)getOut(node.getExpr())).evaluate(globalContext));
+	}
+	
+	public void outAIfthenExpr(AIfthenExpr node) {
+		Value cond = (Value)getOut(node.getRelexpr());
+		Value thenVal = (Value)getOut(node.getThen());
+		Value elseVal = (Value)getOut(node.getElse());
+		setOut(node, new IfThenValue(cond, thenVal, elseVal));
+	}
+	
+	public void outASimpleExpr(ASimpleExpr node) {
+		setOut(node, getOut(node.getSmplexpr()));
+	}
+	
+	public void outARelRelexpr(ARelRelexpr node) {
+		Value a = (Value)getOut(node.getLeft());
+		Value b = (Value)getOut(node.getRight());
+		Value v;
+		if (node.getRelop() instanceof AEqualRelop) {
+			v = new EqualOpValue(a, b);
+		} else if (node.getRelop() instanceof ANoteqRelop) {
+			v = new NotEqualOpValue(a, b);
+		} else if (node.getRelop() instanceof ALessRelop) {
+			v = new LessOpValue(a, b);
+		} else if (node.getRelop() instanceof ALseqRelop) {
+			v = new LessEqualOpValue(a, b);
+		} else if (node.getRelop() instanceof AGreqRelop) {
+			v = new GreaterEqualOpValue(a, b);
+		} else if (node.getRelop() instanceof AGrtrRelop) {
+			v = new GreaterOpValue(a, b);
+		} else {
+			v = null; // To shut Java up
+			System.err.println("Invalid relop: " + node.getRelop().toString());	
+		}
+		setOut(node, v);
+	}
+	
+	public void outATermSmplexpr(ATermSmplexpr node) {
+		setOut(node, getOut(node.getTerm()));
 	}
 	
 	public void outAAddSmplexpr(AAddSmplexpr node) {
@@ -30,14 +78,6 @@ public class Interpreter extends DepthFirstAdapter {
 			System.exit(1);
 		}
 		setOut(node, v);
-	}
-	
-	public void outATermSmplexpr(ATermSmplexpr node) {
-		setOut(node, getOut(node.getTerm()));
-	}
-	
-	public void outAExprFactor(AExprFactor node) {
-		setOut(node, getOut(node.getExpr()));
 	}
 	
 	public void outAFactorTerm(AFactorTerm node) {
@@ -64,29 +104,18 @@ public class Interpreter extends DepthFirstAdapter {
 		setOut(node, v);
 	}
 	
-	public void outARelRelexpr(ARelRelexpr node) {
-		Value a = (Value)getOut(node.getLeft());
-		Value b = (Value)getOut(node.getRight());
-		Value v;
-		if (node.getRelop() instanceof AEqualRelop) {
-			v = new EqualOpValue(a, b);
-		} else if (node.getRelop() instanceof ANoteqRelop) {
-			v = new NotEqualOpValue(a, b);
-		} else if (node.getRelop() instanceof ALessRelop) {
-			v = new LessOpValue(a, b);
-		} else if (node.getRelop() instanceof ALseqRelop) {
-			v = new LessEqualOpValue(a, b);
-		} else if (node.getRelop() instanceof AGreqRelop) {
-			v = new GreaterEqualOpValue(a, b);
-		} else if (node.getRelop() instanceof AGrtrRelop) {
-			v = new GreaterOpValue(a, b);
-		} else {
-			v = null; // To shut Java up
-			System.err.println("Invalid relop: " + node.getRelop().toString());	
-		}
-		setOut(node, v);
+	public void outAExprFactor(AExprFactor node) {
+		setOut(node, getOut(node.getExpr()));
 	}
-
+	
+	public void outAIntFactor(AIntFactor node) {
+		setOut(node, new IntValue(Long.valueOf(node.getIntdenotation().getText())));
+	}
+	
+	public void outARealFactor(ARealFactor node) {
+		setOut(node, new DoubleValue(Double.valueOf(node.getRealdenotation().getText())));
+	}
+	
 	public void outACallFactor(ACallFactor node) {
 		LinkedList<Value> vals = new LinkedList<Value>();
 		PArglst arglst = node.getArglst();
@@ -99,38 +128,5 @@ public class Interpreter extends DepthFirstAdapter {
 			vals.addFirst((Value)getOut(((ASingleArgs)args).getExpr()));
 		}
 		setOut(node, new FuncCallValue(node.getIdent().getText(), vals));
-	}
-	
-	public void outASimpleExpr(ASimpleExpr node) {
-		setOut(node, getOut(node.getSmplexpr()));
-	}
-	
-	public void outADefProg(ADefProg node) {
-		setOut(node, (Value)getOut(node.getDef()));
-	}
-	
-	public void outALetDef(ALetDef node) {
-		LinkedList<String> names = new LinkedList<String>();
-		PParlst parlst = node.getParlst();
-		if (parlst instanceof ANonemptyParlst) {
-			PPars pars = ((ANonemptyParlst)parlst).getPars();
-			while (pars instanceof AMultiPars) {
-				names.addFirst(((AMultiPars)pars).getIdent().getText());
-				pars = ((AMultiPars)pars).getPars();
-			}
-			names.addFirst(((ASinglePars)pars).getIdent().getText());
-		}
-		globalContext.putFuncDef(node.getIdent().getText(), new FuncDef(names, (Value)getOut(node.getExpr())));
-	}
-	
-	public void outAExprComp(AExprComp node) {
-		System.out.println(((Value)getOut(node.getExpr())).evaluate(globalContext));
-	}
-	
-	public void outAIfthenExpr(AIfthenExpr node) {
-		Value cond = (Value)getOut(node.getRelexpr());
-		Value thenVal = (Value)getOut(node.getThen());
-		Value elseVal = (Value)getOut(node.getElse());
-		setOut(node, new IfThenValue(cond, thenVal, elseVal));
 	}
 }
